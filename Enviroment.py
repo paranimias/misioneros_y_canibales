4ae0b02ce923e97dd00b849896cdbb1bec67720c
paranimias <ndceliso@unal.edu.co>
2026-02-19 20:47:15 -0500

    agregadas coordenadas

--- Enviroment.py
+++ Enviroment.py
@@ -5,13 +5,13 @@ import numpy as np
 
 class Environment:
     def __init__(self, monitor):
-            self.monitor = mss.monitors[1]
+        self.monitor = mss.monitors[1]
 
     def screenshot(self):
         with mss() as sct:
             return sct.grab(self.monitor)
 
-    def send(self, action):
+    def response(self, action):
         # Si recibimos una acción del agente enviamos una nueva captura de pantalla, también transformamos su color
         if action:
             return cv2.cvtColor(np.array(self.screenshot()), cv2.COLOR_BGRA2BGR)
