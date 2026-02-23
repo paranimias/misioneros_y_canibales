44aca650cd9386ab3cc9312cbd1ea2b3b8e359b0
paranimias <ndceliso@unal.edu.co>
2026-02-23 08:35:33 -0500

    cambio de initpara recibir argumentos

--- __init__.py
+++ __init__.py
@@ -1,16 +1,21 @@
+import sys
 import agent
 import time
 import environment
 
 if __name__ == "__main__":
-
+    mode = int(sys.argv[1]) if len(sys.argv) > 1 else 1
     e = environment.Environment()
-    e.start_browser()
     a = agent.Agent()
-    game_started = False
-    while not game_started:
-        captura = e.screenshot()
-        game_started = e.start_game(captura)
-        time.sleep(0.5)
-#     time.sleep(5)
-    a.compute(e)
+
+    if mode == 1:
+        e.start_browser()
+        game_started = False
+        while not game_started:
+            captura = e.screenshot()
+            game_started = e.start_game(captura)
+            time.sleep(0.5)
+        a.compute(e)
+    elif mode == 0:
+        time.sleep(5)
+        a.compute(e)
